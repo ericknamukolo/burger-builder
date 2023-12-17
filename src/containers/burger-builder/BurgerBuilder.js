7cf547a9965ae5a7afe5066cb20ff423f3ef0a1d
Erick Namukolo <erickmndev@gmail.com>
2023-12-17 20:25:15 +0200

    init state

--- src/containers/burger-builder/BurgerBuilder.js
+++ src/containers/burger-builder/BurgerBuilder.js
@@ -2,6 +2,14 @@ import React, { Component, Fragment } from 'react';
 import Burger from '../../components/burger/Burger';
 
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 1,
+      bacon: 1,
+      cheese: 1,
+      meat: 1,
+    },
+  };
   render() {
     return (
       <Fragment>
